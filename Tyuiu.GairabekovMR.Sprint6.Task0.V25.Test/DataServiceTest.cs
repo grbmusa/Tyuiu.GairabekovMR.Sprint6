3e2d83b98eab5ab2fff065d340777bc660115b70
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 13:06:46 +0500

    Add project files.

---
+++ Tyuiu.GairabekovMR.Sprint6.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.GairabekovMR.Sprint6.Task0.V25.Lib;
+
+namespace Tyuiu.GairabekovMR.Sprint6.Task0.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = 0.866;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint6.Task0.V25/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GairabekovMR.Sprint6.Task0.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
