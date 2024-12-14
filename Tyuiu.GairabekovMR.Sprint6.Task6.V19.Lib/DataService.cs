dc94be7c2573a5f3b2c561d7d6d7541473247add
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 14:29:05 +0500

    Task6

---
+++ Tyuiu.GairabekovMR.Sprint6.Task6.V19.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.GairabekovMR.Sprint6.Task6.V19.Lib
+{
+    public class DataService : ISprint6Task6V19
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string[] liner;
+            string resStr = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    liner = line.Trim().Split(' ');
+                    foreach (string c in liner)
+                    {
+                        if (c.Contains('l'))
+                        {
+                            resStr += " " + c;
+                        }
+                    }
+
+                }
+            }
+            return resStr.Trim();
+        }
+
+
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint6.Task6.V19.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.GairabekovMR.Sprint6.Task6.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
