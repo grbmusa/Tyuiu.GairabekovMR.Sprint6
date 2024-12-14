82f537c9415bc3bf2f49c6387f0e0a45075b6379
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 14:08:56 +0500

    Task4

---
+++ Tyuiu.GairabekovMR.Sprint6.Task4.V28/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GairabekovMR.Sprint6.Task4.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
